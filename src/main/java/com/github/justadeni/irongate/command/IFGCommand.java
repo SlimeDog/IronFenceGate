7b55c964fc3752ca1c73c2d879acd089a8d6f76c
justADeni <deninikdo@gmail.com>
2023-04-15 15:05:50 +0200

    added command messages, both to command and config

--- src/main/java/com/github/justadeni/irongate/command/IFGCommand.java
+++ src/main/java/com/github/justadeni/irongate/command/IFGCommand.java
@@ -17,23 +17,48 @@ public class IFGCommand implements CommandExecutor {
             if (!sender.hasPermission("ironfencegate.admin"))
                 return true;
 
-        if (args.length == 0 || args.length > 1)
+        if (args.length == 0) {
+            sendMessage(sender, "command.tooshort");
             return true;
+        }
+        if (args.length > 1) {
+            sendMessage(sender, "command.toolong");
+            return true;
+        }
 
         if (args[0].equalsIgnoreCase("get")){
             if (sender instanceof Player p){
+
                 if (p.getInventory().getItemInMainHand().getType().isAir()){
+
                     p.getInventory().setItemInMainHand(Recipe.recipes.get(0).getResult());
+                    sendMessage(sender, "command.itemrecieved");
+
                 } else if (p.getInventory().getItemInOffHand().getType().isAir()){
                     p.getInventory().setItemInOffHand(Recipe.recipes.get(0).getResult());
+                    sendMessage(sender, "command.itemrecieved");
+                } else {
+                    sendMessage(sender, "command.handsfull");
                 }
-                return true;
+            } else {
+                sendMessage(sender, "command.consolecant");
             }
+            return true;
         }
 
-        if (args[0].equalsIgnoreCase("reload"))
+        if (args[0].equalsIgnoreCase("reload")) {
             ConfigManager.get().reload();
+            sendMessage(sender, "command.configreloaded");
+            return true;
+        }
 
         return true;
+    }
+
+    private void sendMessage(CommandSender sender, String path){
+        ConfigManager cm = ConfigManager.get();
+        String message = cm.getStringColors(path);
+        if (!message.equalsIgnoreCase("blank"))
+            sender.sendMessage(message);
     }
 }
